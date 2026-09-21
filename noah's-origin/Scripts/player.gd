2a77e1c15e9ef51034abe518df6aecf82a72598f
qigoh <qigoh1@gmail.com>
2026-09-21 00:35:07 -0700

    Small change

--- noah's-origin/Scripts/player.gd
+++ noah's-origin/Scripts/player.gd
@@ -2,13 +2,18 @@ extends CharacterBody2D
 
 @export var turn_velocity := 0.0
 
-const MAX_TURN_SPEED := 300.0
-const MAX_SPEED := 300.0
+const ACCELERATION := 1000.0
+const MAX_TURN_SPEED := 3000.0
+const BRAKE_ACCEL := 2000.0
+var MAX_SPEED := 300.0
 
+var local_velocity := Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
-	var accel_vs_brake := Input.get_axis("brake", "accelerate") 
-	var local_velocity := Vector2(0.0, -MAX_SPEED)
+	var accel := Input.is_action_pressed("accelerate")
+	var brake := Input.is_action_pressed("brake")
+	
+	local_velocity = Vector2(0.0, move_toward(local_velocity.y, -MAX_SPEED * (1.0 if (accel and (not brake)) else 0.0), ((BRAKE_ACCEL if brake else ACCELERATION) * delta)))
 	
 	var turn_direction := Input.get_axis("turn_left","turn_right")
 	turn_velocity = move_toward(turn_velocity, turn_direction, MAX_TURN_SPEED * delta)
